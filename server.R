364ae628a244f8a64312709e010bc07150eeb61d
Václav Cvrček <vaclav.cvrcek@ff.cuni.cz>
2019-07-14 15:27:30 +0200

    Minor imrovements in Gr (table + help)

--- server.R
+++ server.R
@@ -727,11 +727,28 @@ shinyServer(function(input, output, session) {
           data.frame()
         } else {
           graph.group.data <- doBoot()
+          sketch = htmltools::withTags(table(
+            class = 'display',
+            thead(
+              tr(
+                th(rowspan = 2, 'Skupina', style='text-align:left'),
+                th(rowspan = 2, 'Frekvence'),
+                th(rowspan = 2, 'Podíl'),
+                th(colspan = 2, 'Konfidenční intervaly', style='text-align:center')
+              ),
+              tr(
+                th('Spodní limit'),
+                th('Horní limit')
+                #lapply(rep(c('Length', 'Width'), 2), th)
+              )
+            )
+          ))
+          #print(sketch)
           df <- graph.group.data %>% 
             mutate(Proportion = Fq / sum(Fq)) %>%
             select(Group, Fq, Proportion, Lower, Upper, Reliability)
-          DT::datatable(df, rownames = F, filter="none",
-            colnames = c("Skupina" = 1, "Frekvence" = 2, "Podíl" = 3, "Spodní limit" = 4, "Horní limit" = 5),
+          DT::datatable(df, rownames = F, filter="none", container = sketch,
+            #colnames = c("Skupina" = 1, "Frekvence" = 2, "Podíl" = 3, "Spodní limit" = 4, "Horní limit" = 5),
             options = list(dom = '', ordering=F, columnDefs = list(list(visible=FALSE, targets=5)) )) %>%
             DT::formatRound(1:2, digits = 0) %>%
             DT::formatPercentage(3, digits = 1) %>%
