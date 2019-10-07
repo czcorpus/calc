6c5c1e063acf392bd0af6a3049ce237f19fe319b
Václav Cvrček <vaclav.cvrcek@ff.cuni.cz>
2019-10-07 12:14:54 +0200

    Remove systemd service files

--- run.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-
-# get port number from env var; if unset, a random available port will
-# be used
-port <- as.numeric(Sys.getenv("SHINY_APP_PORT"))
-port <- if (is.na(port)) NULL else port
-shiny::runApp(host = "0.0.0.0", port = port)
